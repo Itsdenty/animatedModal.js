0d76bc0cbbbbb1bfd9b7c8209ca87bc36a176345
Adnan Asani <adnan.asani@commercetools.de>
2015-09-16 10:03:31 +0200

    feature/provide-context: providing context into beforeOpen,beforeClose,afterOpen,afterClose and passed the modal as an argument

--- demo/js/animatedModal.js
+++ demo/js/animatedModal.js
@@ -3,49 +3,52 @@
  * author: João Pereira
  * website: http://www.joaopereira.pt
  * email: joaopereirawd@gmail.com
- * Licensed MIT 
+ * Licensed MIT
 =========================================*/
 
 (function ($) {
- 
+
     $.fn.animatedModal = function(options) {
-        var modal = $(this);
-        
+        var modal = $(this),
+            currentContext = this;
+
         //Defaults
         var settings = $.extend({
-            modalTarget:'animatedModal', 
-            position:'fixed', 
-            width:'100%', 
-            height:'100%', 
-            top:'0px', 
-            left:'0px', 
-            zIndexIn: '9999',  
-            zIndexOut: '-9999',  
-            color: '#39BEB9', 
-            opacityIn:'1',  
-            opacityOut:'0', 
+            modalTarget:'animatedModal',
+            position:'fixed',
+            width:'100%',
+            height:'100%',
+            top:'0px',
+            left:'0px',
+            zIndexIn: '9999',
+            zIndexOut: '-9999',
+            color: '#39BEB9',
+            opacityIn:'1',
+            opacityOut:'0',
             animatedIn:'zoomIn',
             animatedOut:'zoomOut',
-            animationDuration:'.6s', 
-            overflow:'auto', 
+            animationDuration:'.6s',
+            overflow:'auto',
             // Callbacks
-            beforeOpen: function() {},           
-            afterOpen: function() {}, 
-            beforeClose: function() {}, 
+            beforeOpen: function() {},
+            afterOpen: function() {},
+            beforeClose: function() {},
             afterClose: function() {}
- 
-            
 
         }, options);
-        
+
         var closeBt = $('.close-'+settings.modalTarget);
 
-        //console.log(closeBt)
+        ['beforeOpen', 'afterOpen', 'beforeClose', 'afterClose'].forEach(function(key) {
+          if (key in settings) {
+            settings[key] = settings[key].bind(this);
+          }
+        }, this);
 
         var href = $(modal).attr('href'),
             id = $('body').find('#'+settings.modalTarget),
             idConc = '#'+id.attr('id');
-            //console.log(idConc);
+
             // Default Classes
             id.addClass('animated');
             id.addClass(settings.modalTarget+'-off');
@@ -66,7 +69,7 @@
         //Apply stles
         id.css(initStyles);
 
-        modal.click(function(event) {       
+        modal.click(function(event) {
             event.preventDefault();
             $('body, html').css({'overflow':'hidden'});
             if (href == idConc) {
@@ -74,15 +77,15 @@
                     id.removeClass(settings.animatedOut);
                     id.removeClass(settings.modalTarget+'-off');
                     id.addClass(settings.modalTarget+'-on');
-                } 
+                }
 
                  if (id.hasClass(settings.modalTarget+'-on')) {
-                    settings.beforeOpen();
+                    settings.beforeOpen(id);
                     id.css({'opacity':settings.opacityIn,'z-index':settings.zIndexIn});
-                    id.addClass(settings.animatedIn);  
+                    id.addClass(settings.animatedIn);
                     id.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', afterOpen);
-                };  
-            } 
+                };
+            }
         });
 
 
@@ -91,11 +94,11 @@
             event.preventDefault();
             $('body, html').css({'overflow':'auto'});
 
-            settings.beforeClose(); //beforeClose
+            settings.beforeClose(id); //beforeClose
             if (id.hasClass(settings.modalTarget+'-on')) {
                 id.removeClass(settings.modalTarget+'-on');
                 id.addClass(settings.modalTarget+'-off');
-            } 
+            }
 
             if (id.hasClass(settings.modalTarget+'-off')) {
                 id.removeClass(settings.animatedIn);
@@ -105,13 +108,13 @@
 
         });
 
-        function afterClose () {       
+        function afterClose () {
             id.css({'z-index':settings.zIndexOut});
-            settings.afterClose(); //afterClose
+            settings.afterClose(id); //afterClose
         }
 
-        function afterOpen () {       
-            settings.afterOpen(); //afterOpen
+        function afterOpen () {
+            settings.afterOpen(id); //afterOpen
         }
 
     }; // End animatedModal.js
@@ -120,4 +123,4 @@
 
 
 
-        
+
